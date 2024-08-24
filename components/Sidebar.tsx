0a5109164b4f5b17a725c817b157eaff10376d10
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-08-24 20:09:23 +0300

    chore: Add @radix-ui/react-dialog and @radix-ui/react-slot dependencies, update globals.css with new utility classes, and add MobileNav component for mobile navigation

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -12,8 +12,7 @@ const Sidebar = () => {
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((link) => {
-          const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+          const isActive = pathname === link.route || pathname.startsWith(`${link.route}/`);
 
           return (
             <Link
@@ -26,11 +25,11 @@ const Sidebar = () => {
                 }
               )}
             >
-              <Image 
-              src={link.imgUrl}
-              alt={link.label}
-              width={24}
-              height={24}
+              <Image
+                src={link.imgUrl}
+                alt={link.label}
+                width={24}
+                height={24}
               />
               <p className="text-lg font-semibold max-lg:hidden">
                 {link.label}

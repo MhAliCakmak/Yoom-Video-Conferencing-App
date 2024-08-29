816442027965868116cc1f6334455b8f0aebedaa
MhAliCakmak <202803055@ogr.cbu.edu.tr>
2024-08-29 19:19:02 +0300

    chore: Add useToast hook for displaying toast notifications
    ```

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -5,8 +5,10 @@ import { useRouter } from "next/navigation";
 import MeetingModal from "./MeetingModal";
 import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
+import { useToast } from "./ui/use-toast";
 
 const MeetingTypeList = () => {
+  const { toast } = useToast();
   const router = useRouter();
   const { user } = useUser();
   const client = useStreamVideoClient();
@@ -19,33 +21,44 @@ const MeetingTypeList = () => {
     description: "",
     link: "",
   });
-  const [callDetails, setCallDetails] = useState<Call>()
+  const [callDetails, setCallDetails] = useState<Call>();
 
   const createMeeting = async () => {
     if (!client || !user) return;
 
     try {
+      if (!values.dateTime) {
+        toast({
+          title: "Please select a date and time",
+        });
+        return;
+      }
       const id = crypto.randomUUID();
       const call = client.call("default", id);
 
       if (!call) throw new Error("Failed to create call");
-      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+      const startsAt =
+        values.dateTime.toISOString() || new Date(Date.now()).toISOString();
       const description = values.description || "Instant meeting";
-     await call.getOrCreate({
-      data:{
-        starts_at:startsAt,
-        custom:{
-          description
-        }
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          },
+        },
+      });
+      setCallDetails(call);
+
+      if (!values.description) {
+        router.push(`/meeting/${call.id}`);
       }
-     })
-     if(!values.description){
-      router.push(`/meeting/${call.id}`)
-     }
-      setCallDetails(call)
-      setMeetingState(undefined);
+      toast({ title: "Meeting Created" });
     } catch (error) {
       console.log(error);
+      toast({
+        title: "Failed to create meeting",
+      });
     }
   };
   return (
